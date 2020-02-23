c3fff312154b45c76efa5a575be0492313cdf2e3
Nikhil Bharadwaj <bharadwajnikhil77@gmail.com>
2020-02-23 14:33:55 +0530

    Multiplayer movement sync added.

---
+++ Assets/Photon Final/Assets/RW/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+using Photon.Pun;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 10f;
+
+    private void Update()
+    {
+        if (GetComponent<PhotonView>().IsMine)
+        {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            Debug.Log("horizontal : " + horizontal);
+            transform.position += new Vector3(horizontal, 0, 0) * Time.deltaTime * movementSpeed;
+        }
+    }
+}
