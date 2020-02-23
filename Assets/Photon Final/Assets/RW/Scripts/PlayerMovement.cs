b6816fc55e98f64cd888ef63a42e63b9dafcbb8f
Nikhil Bharadwaj <bharadwajnikhil77@gmail.com>
2020-02-23 16:59:27 +0530

    Added attack animation.

--- Assets/Photon Final/Assets/RW/Scripts/PlayerMovement.cs
+++ Assets/Photon Final/Assets/RW/Scripts/PlayerMovement.cs
@@ -7,15 +7,19 @@ using Photon.Pun;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float movementSpeed = 10f;
+    public string player;
 
     private void Update()
     {
         if (GetComponent<PhotonView>().IsMine)
         {
             float horizontal = Input.GetAxisRaw("Horizontal");
-            Debug.Log("horizontal : " + (int)horizontal);
-            transform.position += new Vector3(horizontal, 0, 0) * Time.deltaTime * movementSpeed;
-            GetComponentInChildren<Animator>().SetInteger("Speed", (int)horizontal); 
+            if(!GetComponentInChildren<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+                transform.position += new Vector3(horizontal, 0, 0) * Time.deltaTime * movementSpeed;
+            GetComponentInChildren<Animator>().SetInteger("Speed", (int)horizontal);
+
+            if (Input.GetKeyDown(KeyCode.Space))
+                GetComponentInChildren<Animator>().SetTrigger("Attack");
         }
     }
 }
