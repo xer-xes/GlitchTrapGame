cc0c4c17a22f5769e6c5e2f4ff5c85a93e92a4b4
Nikhil Bharadwaj <bharadwajnikhil77@gmail.com>
2020-02-28 15:57:11 +0530

    Added camera movement.

---
+++ Assets/Photon Final/Assets/RW/Scripts/CameraMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private GameObject camera;
+
+    private void Start()
+    {
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    private void Update()
+    {
+        if (GetComponent<PhotonView>().IsMine)
+            camera.transform.position = new Vector3(transform.position.x, 0, -10);
+    }
+}
---
+++ Assets/Photon Final/Assets/RW/Scripts/CreepManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CreepManager : MonoBehaviour
+{
+    [SerializeField] private GameObject rightMeleeSpawn;
+    [SerializeField] private GameObject rightRangeSpawn;
+    [SerializeField] private GameObject leftMeleeSpawn;
+    [SerializeField] private GameObject leftRangeSpawn;
+
+    [SerializeField] private GameObject rightCreep;
+    [SerializeField] private GameObject leftCreep;
+}
