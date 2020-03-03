437c2f2a59053c9632c312eef5ad81b3f53b222c
Nikhil Bharadwaj <bharadwajnikhil77@gmail.com>
2020-03-03 20:55:23 +0530

    Left Range creep added with animation sync.

--- Assets/Photon Final/Assets/RW/Scripts/CreepManager.cs
+++ Assets/Photon Final/Assets/RW/Scripts/CreepManager.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using Photon.Realtime;
+using Photon.Pun;
 
 public class CreepManager : MonoBehaviour
 {
@@ -7,6 +9,18 @@ public class CreepManager : MonoBehaviour
     [SerializeField] private GameObject leftMeleeSpawn;
     [SerializeField] private GameObject leftRangeSpawn;
 
-    [SerializeField] private GameObject rightCreep;
-    [SerializeField] private GameObject leftCreep;
+    [SerializeField] private GameObject meleeRightCreep;
+    [SerializeField] private GameObject rangeRightCreep;
+    [SerializeField] private GameObject meleeLeftCreep;
+    [SerializeField] private GameObject rangeLeftCreep;
+
+    private void Start()
+    {
+        Invoke("SpawnCreeps", 10);
+    }
+
+    private void SpawnCreeps()
+    {
+        GameObject rangeLeft = PhotonNetwork.Instantiate("RangeLeftCreep", leftRangeSpawn.transform.position, Quaternion.identity, 1);
+    }
 }
---
+++ Assets/Photon Final/Assets/RW/Scripts/CreepsBehaviourScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+
+public class CreepsBehaviourScript : MonoBehaviour
+{
+    public string type;
+    public bool isAttacking = false;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (this.gameObject.tag == "Left")
+        {
+            if (collision.gameObject.tag == "Right" ||
+                 (collision.gameObject.tag == "Player" && collision.gameObject.GetComponent<PlayerMovement>().player == "Player2"))
+            {
+                isAttacking = true;
+                if (collision.gameObject.tag == "Right")
+                    collision.gameObject.GetComponent<CreepsBehaviourScript>().isAttacking = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (this.gameObject.tag == "Left")
+        {
+            if (collision.gameObject.tag == "Right" ||
+                (collision.gameObject.tag == "Player" && collision.gameObject.GetComponent<PlayerMovement>().player == "Player2"))
+            {
+                isAttacking = false;
+                if (collision.gameObject.tag == "Right")
+                    collision.gameObject.GetComponent<CreepsBehaviourScript>().isAttacking = false;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        GetComponent<Animator>().SetBool("Attack", isAttacking);
+    }
+
+    public void LaunchBullet()
+    {
+
+    }
+}
